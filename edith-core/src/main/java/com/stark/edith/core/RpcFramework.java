fd5c869ef5b4001c913604b11b3f646347b1ead9
fuyongde <fuyongde@zhuifintech.com>
2019-07-15 16:54:33 +0800

    :art:: 优化多服务暴露时的代码结构

--- edith-core/src/main/java/com/stark/edith/core/RpcFramework.java
+++ edith-core/src/main/java/com/stark/edith/core/RpcFramework.java
@@ -1,5 +1,7 @@
 package com.stark.edith.core;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
 import com.stark.edith.core.util.CollectionUtils;
 import com.stark.edith.core.util.StringUtils;
 import com.stark.edith.core.util.ThreadUtils;
@@ -12,8 +14,10 @@ import java.lang.reflect.Proxy;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Stream;
 
 import static com.stark.edith.core.constant.RpcConstants.PORT_MAX;
 import static com.stark.edith.core.constant.RpcConstants.PORT_MIN;
@@ -26,6 +30,8 @@ public class RpcFramework {
 
     private static final ExecutorService executorService = ThreadUtils.newCachedThreadPool(0);
 
+    private static final Map<String, Object> serviceMap = Maps.newHashMap();
+
     public static void export(final List<Object> services, int port) throws Exception {
         if (CollectionUtils.isEmpty(services)) {
             throw new IllegalArgumentException("Collection services is empty");
@@ -34,25 +40,26 @@ public class RpcFramework {
             throw new IllegalArgumentException("Invalid port " + port);
         }
         services.forEach(service -> System.out.println("Export service " + service.getClass().getName() + " on port " + port));
+        services.forEach(service -> Stream.of(service.getClass().getInterfaces())
+                .forEach(o -> serviceMap.put(o.getName(), service)));
+
         ServerSocket server = new ServerSocket(port);
         for (; ; ) {
             Socket socket = server.accept();
             executorService.execute(() -> {
                 try {
                     try (ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
-                        String methodName = input.readUTF();
+                        String interfaceAndMethod = input.readUTF();
+                        String interfaceName = JSON.parseObject(interfaceAndMethod).getString("interfaceName");
+                        String methodName = JSON.parseObject(interfaceAndMethod).getString("methodName");
                         Class<?>[] parameterTypes = (Class<?>[]) input.readObject();
                         Object[] arguments = (Object[]) input.readObject();
                         ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
                         try {
-                            for (Object service : services) {
-                                try {
-                                    Method method = service.getClass().getMethod(methodName, parameterTypes);
-                                    Object result = method.invoke(service, arguments);
-                                    output.writeObject(result);
-                                } catch (NoSuchMethodException ignored) {
-                                }
-                            }
+                            Object service = serviceMap.get(interfaceName);
+                            Method method = service.getClass().getMethod(methodName, parameterTypes);
+                            Object result = method.invoke(service, arguments);
+                            output.writeObject(result);
                         } catch (Throwable t) {
                             output.writeObject(t);
                         } finally {
@@ -89,7 +96,10 @@ public class RpcFramework {
         return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[]{interfaceClass}, (proxy, method, arguments) -> {
             try (Socket socket = new Socket(host, port)) {
                 try (ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream())) {
-                    output.writeUTF(method.getName());
+                    Map<String, String> interfaceAndMethod = Maps.newHashMap();
+                    interfaceAndMethod.put("interfaceName", interfaceClass.getName());
+                    interfaceAndMethod.put("methodName", method.getName());
+                    output.writeUTF(JSON.toJSONString(interfaceAndMethod));
                     output.writeObject(method.getParameterTypes());
                     output.writeObject(arguments);
                     try (ObjectInputStream input = new ObjectInputStream(socket.getInputStream())) {
